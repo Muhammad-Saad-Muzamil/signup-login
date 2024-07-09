10d7fe34d112cbd028721e4f24d879e63b232474
Muhammad Saad Muzamil <muhammadsaadmuzamil@gmail.com>
2024-07-09 11:30:54 -0700

    initial commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(300, 196, 179, 179),
+      body: Column(children: [
+        //logo
+
+       // welcome back you have been missed 
+
+
+        //password textfield 
+        // forgetpassword
+
+        // sign in button
+
+
+
+
+      ],),
+
+    );
+  }
+} 
